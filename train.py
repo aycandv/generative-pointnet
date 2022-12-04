d1d802322c245b6373653845153031f08d67cf81
Aycan Deniz Vit <aycandv@gmail.com>
2022-12-05 00:17:14 +0300

    refactor: add logging

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
+import os
 import torch
 import argparse
+import logging
 from tqdm import tqdm
+from termcolor import colored
 from pathlib import Path
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -27,6 +30,8 @@ def train(args):
     # Create loss function
     criterion = pointnetloss
 
+    best_acc = 0
+
     for epoch in range(args.epochs): 
         model.train()
         running_loss = 0.0
@@ -34,7 +39,7 @@ def train(args):
         total = 0
 
         # pbar
-        pbar = tqdm(train_loader, total=len(train_loader), leave=True, unit="batch", ascii=True, position=0, desc='Epoch {:4d}'.format(epoch+1), ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}], {rate_fmt}{postfix}')
+        pbar = tqdm(train_loader, total=len(train_loader), leave=True, unit="batch", ascii=True, position=0, desc='Epoch {:4d}'.format(epoch+1), ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}], {rate_fmt} {postfix}')
         for i, data in enumerate(pbar):
             inputs, labels = data['pointcloud'].to(args.device).float(), data['category'].to(args.device)
             optimizer.zero_grad()
@@ -43,15 +48,17 @@ def train(args):
             loss = criterion(outputs, labels, m3x3, m64x64)
             loss.backward()
             optimizer.step()
-            pbar.set_postfix(loss=loss.item())
+            
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
+            pbar.set_postfix({'loss': '%.3f' % (running_loss / 10), 'acc': '%.3f' % (100 * correct / total)})
+
             # print statistics
             running_loss += loss.item()
             if i % 10 == 9:    # print every 10 mini-batches
-                pbar.set_postfix(loss=running_loss / 10, acc=correct/total)
+                pbar.set_postfix({'loss': '%.3f' % (running_loss / 10), 'acc': '%.3f' % (100 * correct / total)})
                 running_loss = 0.0
 
         model.eval()
@@ -69,19 +76,37 @@ def train(args):
                     correct += (predicted == labels).sum().item()
                     pbar_val.set_postfix(acc=correct/total)
             val_acc = 100. * correct / total
+
             pbar_val.set_postfix(acc=val_acc)
+
+        
+        # Save best model
+        if val_acc > best_acc:
+            best_acc = val_acc
+            if not os.path.exists(args.save_dir):
+                os.makedirs(args.save_dir, exist_ok=True)
+            torch.save(model.state_dict(), args.save_dir + "/best_model.pth")
+            # log best model info with green color
+            logging.info(colored(f"Best model saved at epoch {epoch+1} with acc {best_acc}", "green"))
+
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('--root_dir', type=str, default='data/ModelNet10')
-    args.add_argument('--batch_size', type=int, default=3, help='input batch size')
+    args.add_argument('--root_dir', type=str, default='data/ModelNet10', help='path to dataset')
+    args.add_argument('--batch_size', type=int, default=4, help='input batch size')
     args.add_argument('--num_workers', type=int, default=0, help='number of workers')
     args.add_argument('--epochs', type=int, default=6, help='number of epochs to train for')
-    args.add_argument('--lr', type=float, default=0.001)
-    args.add_argument('--momentum', type=float, default=0.9)
-    args.add_argument('--weight_decay', type=float, default=0.0005)
-    args.add_argument('--device', type=str, default='cuda')
+    args.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    args.add_argument('--momentum', type=float, default=0.9, help='momentum')
+    args.add_argument('--weight_decay', type=float, default=0.0005, help='weight decay')
+    args.add_argument('--device', type=str, default='cuda', help='device to use')
+    args.add_argument('--save_dir', type=str, default='checkpoints', help='path to save model')
     args = args.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    # log hyperparameters
+    logging.info(args)
+
 
     train(args)
